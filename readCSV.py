2020c54a504d6aabcaa170067fe3ffdc789e3bf3
Loulou <louisarna@gmail.com>
2024-02-15 11:27:03 +0100

    Add csvFile and change readCSV

--- readCSV.py
+++ readCSV.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # IMPORTANT: changer le chemin du fichier avec celui de votre local pour que ça fonctionne
-df = pd.read_csv('../../prix_immobilier_fictif.csv', sep=',')
+df = pd.read_csv('prix_immobilier_fictif.csv', sep=',')
 
 def loadAllCitiesAvailable():
     return df['Ville'].unique().tolist()
